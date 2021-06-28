f37ad558f0e5394500cadf0b253e7da6aa9c9bb2
Gautham Chengappa <gautham.hc@gmail.com>
2021-06-28 12:56:45 +0530

    inital webpage series-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App">Hello</div>;
-}
+import React from "react";
+import "./default.scss";
+import Header from "./components/Header";
+import Homepage from "./components/pages/Homepage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
+};
 
 export default App;
