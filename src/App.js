98616246bcf75bedff0ed1d8db9bf7f8c77d627c
Gautham Chengappa <gautham.hc@gmail.com>
2021-06-28 14:44:06 +0530

    section3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from "react";
 import "./default.scss";
 import Header from "./components/Header";
 import Homepage from "./components/pages/Homepage";
+import Registration from "./components/pages/Registration";
+import { Switch, Route } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <MainLayout>
+              <Homepage />
+            </MainLayout>
+          )}
+        />
+        <Route
+          exact
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./styles.scss";
 import logo from "./../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={logo} alt="Logo SimpleTut" />
+          <Link to="/">
+            <img src={logo} alt="Logo SimpleTut" />
+          </Link>
+        </div>
+
+        <div className="callToActions">
+          <ul>
+            <Link to="/registration">Register</Link>
+          </ul>
         </div>
       </div>
     </header>
